f2f76c2a6f34409316b260af70f4e95063dde991
mosguinz <mos.guinz@gmail.com>
2024-06-08 14:43:54 -0700

    Make `Compliance` a field of `Student

--- zipfile_check.py
+++ zipfile_check.py
@@ -15,20 +15,25 @@ CANVAS_TOKEN = "1234"
 
 
 @dataclass
+class Compliance:
+    folders: bool = None
+    report_name: bool = None
+    zipfile_name: bool = None
+
+    def __bool__(self):
+        return all((self.folders, self.report_name, self.zipfile_name))
+
+
+@dataclass
 class Student:
     name: str
     canvas_id: str
     sis_id: str
     filename: str
+    compliance: Compliance
 
     def __hash__(self):
         return hash(self.sis_id)
-
-
-@dataclass
-class Compliance:
-    folders: bool = None
-    report: bool = None
 
 
 def cleanup_files(path):
@@ -67,12 +72,18 @@ def check_report(temp_dir: str) -> bool:
 
 
 def check_zipfile(canvas_zip, parts: list[str] = None, report=True, debug=True):
-    compliance = {}
+    students = []
+
     with zipfile.ZipFile(canvas_zip, "r") as zf:
         for submission in zf.infolist():
             res = re.match(r"([^\W_]+)(?:_\w+)*_(\d+)_(\d+)_(.+)", submission.filename)
-            student = Student(*res.groups())
-            compliance[student] = Compliance()
+            original_filename = res[4]
+            compliance = Compliance()
+
+            if re.match(r"\w+-Assignment-\w+(-\d)?\.zip", original_filename):
+                compliance.zipfile_name = True
+            else:
+                compliance.zipfile_name = False
 
             with tempfile.TemporaryDirectory() as temp_dir:
                 with zipfile.ZipFile(zf.open(submission)) as student_zip:
@@ -116,3 +127,10 @@ def send_message(assignment_name: str, canvas_token=None, debug=True):
     ) as req:
         pprint(req.headers)
         pprint(req.json())
+
+
+if __name__ == '__main__':
+    students = check_zipfile("submissions.zip", ["c", "d"])
+    for student in students:
+        if not student.compliance:
+            pprint(student)
